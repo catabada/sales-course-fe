186bf4653e4324b43ceaa7b100cb2fef8d4574b7
Huu An <81819172+catabada@users.noreply.github.com>
2022-11-04 15:13:47 +0700

    edit code

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -1,35 +1,60 @@
 import styles from './Breadcrumb.module.scss';
 import classNames from 'classnames/bind';
-import { Box, CardContent, Grid, Paper, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import {Box, Breadcrumbs, CardContent, Container, Grid, Link, Paper, Typography} from '@mui/material';
+import {NavigateNext} from "@mui/icons-material";
 
 
 const cx = classNames.bind(styles);
-function Breadcrumb({ data }) {
-	// const location = useLocation();
 
-	return (
-		<Box className={cx('breadcrumb')}>
-			<Paper
-				className={cx('image-bg')}
-				style={{ backgroundImage: `linear-gradient(0, rgba(0, 0, 0, 0.75) 0%, rgba(0, 0, 0, 0) 100%), url(${data.url})` }}
-			>
-				<div className={cx('content')}>
-					<div className={cx('nav')}>
-						<ul>
-							<li>
-								<Box component={Link} to={'/'}></Box>
-							</li>
-						</ul>
-					</div>
+function Breadcrumb({data}) {
+    const breadcrumbs = [
+        <Link key="1" color="inherit" href="/"
+            // onClick={handleClick}
+        >
+            Trang chủ
+        </Link>,
+        <Link
+            key="2"
+            color="inherit"
+            to="/"
+            // onClick={handleClick}
+        >
+            Ngoại ngữ
+        </Link>,
+        <Link key="3" color="text.primary">
+            Breadcrumb
+        </Link>,
+    ];
 
-					<CardContent >
+    return (
+        <Box className={cx('breadcrumb')}>
+            <Paper
+                className={cx('image-bg')}
+                style={{backgroundImage: `linear-gradient(0, rgba(0, 0, 0, 0.75) 0%, rgba(0, 0, 0, 0) 100%), url(${data.url})`}}
+            >
+                <Container maxWidth={false}>
+                    <div className={cx('content')}>
+                        <Breadcrumbs
+                            separator={<NavigateNext fontSize="medium"/>}
+                            aria-label="breadcrumb"
+                        >
+                            {breadcrumbs}
+                        </Breadcrumbs>
 
-						Ngoại Ngữ
-					</CardContent>
-				</div>
-			</Paper>
-		</Box >
-	);
+                        <Typography variant="h2"
+                                    sx={{
+                                        fontWeight: 'bold',
+										color: '#fff',
+										margin: "10px 0 0 20px"
+                                    }}>
+                            Ngoại Ngữ
+                        </Typography>
+                    </div>
+                </Container>
+
+            </Paper>
+        </Box>
+    );
 }
+
 export default Breadcrumb;
