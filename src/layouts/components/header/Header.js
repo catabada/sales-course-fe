7e1f41ffb3f2b740ec0fa95f2ca456763162d3ae
19130017 <93698875+19130017@users.noreply.github.com>
2022-12-21 08:13:05 +0700

    fix header UI

--- src/layouts/components/header/Header.js
+++ src/layouts/components/header/Header.js
@@ -3,86 +3,80 @@ import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import Search from '../search';
 import Category from '../category';
-import {
-    AppBar,
-    Box,
-    Button,
-    Container,
-    Toolbar,
-    Typography
-} from '@mui/material';
+import { AppBar, Box, Button, Container, Toolbar, Typography } from '@mui/material';
 import { LogoIcon } from '~/components/icons';
 import PlayIcon from '@mui/icons-material/PlayArrowOutlined';
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from 'react';
 
 // layouts
-import HeaderActive from "~/layouts/components/header/HeaderActive";
-import HeaderAccount from "~/layouts/components/header/HeaderAccount";
-import CartHeader from "~/layouts/components/header/CartHeader";
+import HeaderActive from '~/layouts/components/header/HeaderActive';
+import HeaderAccount from '~/layouts/components/header/HeaderAccount';
+import CartHeader from '~/layouts/components/header/CartHeader';
 import { useDispatch, useSelector } from 'react-redux';
 import { requestGetProfile } from '~/redux/user/userSlice';
 
 const cx = classNames.bind(styles);
 
 function Header() {
-    const dispatch = useDispatch()
-    const { userId, accessToken, imageUrl, firstName } = useSelector(state => state.authReducer)
-    const {user} = useSelector(state => state.userReducer)
+    const dispatch = useDispatch();
+    const { userId, accessToken, imageUrl, firstName } = useSelector((state) => state.authReducer);
+    const { user } = useSelector((state) => state.userReducer);
     const navigate = useNavigate();
 
     useEffect(() => {
         if (userId !== 0) {
-            dispatch(requestGetProfile({ userId: userId, accessToken: accessToken }))
+            dispatch(requestGetProfile({ userId: userId, accessToken: accessToken }));
         }
-
-    }, [dispatch, userId, accessToken])
+    }, [dispatch, userId, accessToken]);
 
     return (
         <AppBar position={'fixed'} sx={{ zIndex: 1000 }}>
             <Container maxWidth={false} className={cx('wrapper')}>
                 <Toolbar className={cx('inner')}>
                     <div className={cx('header')}>
-                        <Box onClick={() => navigate('/')}
-                            className={cx('logo')}>
-                            <LogoIcon className={cx('icon')} />
+                        <Box onClick={() => navigate('/')} className={cx('logo', 'col-1')}>
+                            <p>
+                                <span>S</span>Course
+                            </p>
                         </Box>
                         <Category />
 
                         <Search />
                         <HeaderActive />
-                        {
-                            (!!user) &&
-                            <Box component={Link} to={'/my-courses'} className={cx('my-courses')}>
+                        {!!user && (
+                            <Box component={Link} to={'/my-courses'} className={cx('my-courses', 'col-2')}>
                                 <PlayIcon sx={{ height: '3rem', width: '3rem' }} />
                                 <Typography className={cx('text')}>Khoá học của tôi</Typography>
                             </Box>
-                        }
+                        )}
                         <CartHeader />
-                        {
-                            (!!user) ?
-                                <HeaderAccount image={imageUrl} />
-                                :
-                                <>
-                                    <Box onClick={() => navigate('/auth/signin')}
-                                        className={cx('wrapper-link')}>
-                                        <Button variant="text" className={cx('btn', 'btn-text')}>
-                                            Đăng nhập
-                                        </Button>
-                                    </Box>
-                                    <Box onClick={() => navigate('/auth/signup')}
-                                        className={cx('wrapper-link')}>
-                                        <Button variant="outline" className={cx('btn', 'btn-primary')}>
-                                            Đăng ký
-                                        </Button>
-                                    </Box>
-                                </>
-                        }
+                        {!!user ? (
+                            <HeaderAccount image={imageUrl} />
+                        ) : (
+                            <>
+                                <Box className={cx('wrapper-link')}>
+                                    <Button
+                                        onClick={() => navigate('/auth/signin')}
+                                        variant="text"
+                                        className={cx('btn', 'btn-text')}
+                                    >
+                                        Đăng nhập
+                                    </Button>
+                                    <Button
+                                        onClick={() => navigate('/auth/signup')}
+                                        variant="outline"
+                                        className={cx('btn', 'btn-primary')}
+                                    >
+                                        Đăng ký
+                                    </Button>
+                                </Box>
+                            </>
+                        )}
                     </div>
                 </Toolbar>
             </Container>
         </AppBar>
-    )
-        ;
+    );
 }
 
 export default Header;
--- src/layouts/components/header/HeaderActive.js
+++ src/layouts/components/header/HeaderActive.js
@@ -1,63 +1,62 @@
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, InputBase, TextField, Typography } from "@mui/material";
-import { LogoIcon } from "~/components/icons";
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    InputBase,
+    TextField,
+    Typography,
+} from '@mui/material';
+import { LogoIcon } from '~/components/icons';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import MySwal from "~/constants/MySwal";
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import MySwal from '~/constants/MySwal';
 import { useNavigate } from 'react-router-dom';
-import { Form, useForm } from "~/hooks/useForm";
-import { requestActiveCourse } from "~/redux/auth/authSlice";
-import Loading from "~/components/loading/Loading";
-
+import { Form, useForm } from '~/hooks/useForm';
+import { requestActiveCourse } from '~/redux/auth/authSlice';
+import Loading from '~/components/loading/Loading';
 
 const cx = classNames.bind(styles);
 
 function HeaderActive() {
     const [open, setOpen] = useState(false);
-    const { user } = useSelector(state => state.userReducer)
-    const { isLoading } = useSelector(state => state.authReducer)
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+    const { user } = useSelector((state) => state.userReducer);
+    const { isLoading } = useSelector((state) => state.authReducer);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-    const { accessToken, response } = useSelector(state => state.authReducer)
-
+    const { accessToken, response } = useSelector((state) => state.authReducer);
 
     const initialValues = {
         code: '',
-    }
+    };
     const validate = (fieldValues = values) => {
-        let temp = { ...errors }
-        let tempEnable = { ...errorsEnable }
+        let temp = { ...errors };
+        let tempEnable = { ...errorsEnable };
         if ('code' in fieldValues) {
             if (fieldValues.code === '') {
                 tempEnable.code = true;
-                temp.code = 'Không được để trống.'
+                temp.code = 'Không được để trống.';
             } else {
                 tempEnable.code = false;
-                temp.code = ''
+                temp.code = '';
             }
         }
         setErrors({
-            ...temp
-        })
+            ...temp,
+        });
         setErrorsEnable({
-            ...tempEnable
-        })
-        if (fieldValues === values)
-            return Object.values(temp).every(x => x === "")
-    }
+            ...tempEnable,
+        });
+        if (fieldValues === values) return Object.values(temp).every((x) => x === '');
+    };
 
-    const {
-        values,
-        setValues,
-        errors,
-        setErrors,
-        errorsEnable,
-        setErrorsEnable,
-        handleInputChange,
-        resetForm
-    } = useForm(initialValues, true, validate);
+    const { values, setValues, errors, setErrors, errorsEnable, setErrorsEnable, handleInputChange, resetForm } =
+        useForm(initialValues, true, validate);
 
     const handleClickOpen = () => {
         if (!!user) {
@@ -71,9 +70,9 @@ function HeaderActive() {
                 cancelButtonText: 'Hủy',
             }).then((result) => {
                 if (result.isConfirmed) {
-                    navigate('/auth/signin')
+                    navigate('/auth/signin');
                 }
-            })
+            });
         }
     };
 
@@ -84,114 +83,130 @@ function HeaderActive() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        await dispatch(requestActiveCourse({ code: values.code, accessToken: accessToken }))
-        handleClose()
-        resetForm()
-    }
+        await dispatch(requestActiveCourse({ code: values.code, accessToken: accessToken }));
+        handleClose();
+        resetForm();
+    };
 
-    return <Box component="form" onSubmit={handleSubmit}>
-        <Box sx={{ flex: 1 }}>
-            <Button variant="outlined" className={cx('btn-active')} onClick={handleClickOpen}>
-                Kích hoạt khoá học
-            </Button>
-            <Dialog open={open} onClose={handleClose}
-                PaperProps={{
-                    style: {
-                        maxWidth: 600,
-                        minWidth: 520,
-                    }
-                }}>
-                <DialogTitle sx={{ textAlign: 'center', position: 'relative' }}>
-                    <LogoIcon />
-                </DialogTitle>
-                <DialogContent>
-                    <Box sx={{ marginBottom: '1rem' }}>
-                        <Typography variant='body1'
+    return (
+        <Box
+            component="form"
+            onSubmit={handleSubmit}
+            className="col-2"
+            sx={{ display: 'flex', justifyContent: 'center' }}
+        >
+            <Box>
+                <Button variant="outlined" className={cx('btn-active')} onClick={handleClickOpen}>
+                    Kích hoạt khoá học
+                </Button>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                        style: {
+                            maxWidth: 600,
+                            minWidth: 520,
+                        },
+                    }}
+                >
+                    <DialogTitle sx={{ textAlign: 'center', position: 'relative' }}>
+                        <LogoIcon />
+                    </DialogTitle>
+                    <DialogContent>
+                        <Box sx={{ marginBottom: '1rem' }}>
+                            <Typography
+                                variant="body1"
+                                sx={{
+                                    textAlign: 'center',
+                                    fontSize: '3rem',
+                                    lineHeight: '4rem',
+                                    fontWeight: 'bold',
+                                }}
+                            >
+                                Kích hoạt khóa học của bạn
+                            </Typography>
+                            <Typography
+                                variant="body1"
+                                sx={{ textAlign: 'center', fontSize: '1.6rem', lineHeight: '2rem' }}
+                            >
+                                Nhập mã kích hoạt của bạn vào ô dưới đây
+                            </Typography>
+                        </Box>
+                        <TextField
+                            placeholder="Nhập mã kích hoạt"
+                            fullWidth
                             sx={{
-                                textAlign: 'center',
-                                fontSize: '3rem',
-                                lineHeight: '4rem',
-                                fontWeight: 'bold'
-                            }}>
-                            Kích hoạt khóa học của bạn
-                        </Typography>
-                        <Typography variant='body1'
-                            sx={{ textAlign: 'center', fontSize: '1.6rem', lineHeight: '2rem', }}>
-                            Nhập mã kích hoạt của bạn vào ô dưới đây
-                        </Typography>
-                    </Box>
-                    <TextField
-                        placeholder='Nhập mã kích hoạt'
-                        fullWidth
-                        sx={{
-                            width: '100%',
-                            outline: 'none',
-                            border: 'none',
-                            backgroundColor: "#eee",
-                            height: 64,
-                            borderRadius: '1rem',
-                            marginBottom: '3rem',
-                            fontSize: '2rem',
-                        }}
-                        name='code'
-                        value={values.code}
-                        onChange={handleInputChange}
-                        error={errorsEnable.code}
-                        helperText={errors.code}
-                        FormHelperTextProps={{ style: { fontSize: 12 } }}
-                        inputProps={{
-                            style: {
-                                textAlign: 'center',
-                                fontSize: '2.4rem'
-                            }
-                        }}
-                    />
-                    <Button
-                        type='submit'
-                        sx={{
-                            width: '100%',
-                            height: 64,
-                            backgroundColor: '#C89F65',
-                            color: '#fff',
-                            borderRadius: '1rem',
-                            fontSize: '1.8rem',
-                            textTransform: 'initial',
-                            '&:hover': {
+                                width: '100%',
+                                outline: 'none',
+                                border: 'none',
+                                backgroundColor: '#eee',
+                                height: 64,
+                                borderRadius: '1rem',
+                                marginBottom: '3rem',
+                                fontSize: '2rem',
+                            }}
+                            name="code"
+                            value={values.code}
+                            onChange={handleInputChange}
+                            error={errorsEnable.code}
+                            helperText={errors.code}
+                            FormHelperTextProps={{ style: { fontSize: 12 } }}
+                            inputProps={{
+                                style: {
+                                    textAlign: 'center',
+                                    fontSize: '2.4rem',
+                                },
+                            }}
+                        />
+                        <Button
+                            type="submit"
+                            sx={{
+                                width: '100%',
+                                height: 64,
                                 backgroundColor: '#C89F65',
                                 color: '#fff',
-                            },
-                            marginBottom: '3rem'
-                        }}
-                        onClick={handleSubmit}
+                                borderRadius: '1rem',
+                                fontSize: '1.8rem',
+                                textTransform: 'initial',
+                                '&:hover': {
+                                    backgroundColor: '#C89F65',
+                                    color: '#fff',
+                                },
+                                marginBottom: '3rem',
+                            }}
+                            onClick={handleSubmit}
+                        >
+                            Kích hoạt khóa học của bạn
+                        </Button>
 
-                    >Kích hoạt khóa học của bạn</Button>
-
-                    <DialogActions sx={{ justifyContent: 'center' }}>
-                        <Box>
-                            <Typography variant='body1' sx={{
-                                margin: '1rem 0',
-                                fontSize: '1.2rem',
-                                lineHeight: '2.2rem'
-                            }}>
-                                Nếu bạn có gặp vấn đề về kích hoạt tài khoản, xin vui lòng liên hệ
-                                hotline:
-                            </Typography>
-                            <Box sx={{ textAlign: 'center' }}>
-                                <Box component={"span"} sx={{ color: '#C89F65', fontSize: '1.2rem' }}>
-                                    1800 6816
-                                </Box>
-                                <Box component={'span'} sx={{ fontSize: '1.2rem', marginLeft: '0.3rem' }}>
-                                    (07:00 - 24:00 các ngày trong tuần)
+                        <DialogActions sx={{ justifyContent: 'center' }}>
+                            <Box>
+                                <Typography
+                                    variant="body1"
+                                    sx={{
+                                        margin: '1rem 0',
+                                        fontSize: '1.2rem',
+                                        lineHeight: '2.2rem',
+                                    }}
+                                >
+                                    Nếu bạn có gặp vấn đề về kích hoạt tài khoản, xin vui lòng liên hệ hotline:
+                                </Typography>
+                                <Box sx={{ textAlign: 'center' }}>
+                                    <Box component={'span'} sx={{ color: '#C89F65', fontSize: '1.2rem' }}>
+                                        1800 6816
+                                    </Box>
+                                    <Box component={'span'} sx={{ fontSize: '1.2rem', marginLeft: '0.3rem' }}>
+                                        (07:00 - 24:00 các ngày trong tuần)
+                                    </Box>
                                 </Box>
                             </Box>
-                        </Box>
-                    </DialogActions>
-                </DialogContent>
-            </Dialog>
+                        </DialogActions>
+                    </DialogContent>
+                </Dialog>
+            </Box>
+            <Loading open={isLoading} />
         </Box>
-        <Loading open={isLoading} />
-    </Box>
-
+    );
 }
 
-export default HeaderActive
+export default HeaderActive;
